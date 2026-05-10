56ca5c501e286c7b21d7395b5127828aadbd323c
het <12867519+het7854@user.noreply.gitee.com>
2026-05-10 17:47:30 +0800

    Prepare Homebrew release assets.

--- newsy.rb
+++ newsy.rb
@@ -2,7 +2,7 @@ class Newsy < Formula
   desc "Terminal news reader with RSS and plugin-based sources"
   homepage "https://github.com/876380496/newsy"
   url "https://github.com/876380496/newsy/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_REAL_SHA256"
+  sha256 "bd0b83fc87f96c6eacff8509372c75d5b1074d7c994acce71ffef6c7068516d1"
   license "MIT"
 
   depends_on "go" => :build
